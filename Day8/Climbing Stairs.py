9ac3fd3911aec99c2b7353c2d1141278a270f566
HARINI <harini@HARINIs-MacBook-Air.local>
2026-07-02 10:36:21 +0530

    Dsa Day1 to 10 leetcode questions

---
+++ Day8/Climbing Stairs.py
@@ -0,0 +1,13 @@
+def climbStairs(n):
+    if n <= 2:
+        return n
+
+    a, b = 1, 2
+
+    for i in range(3, n + 1):
+        a, b = b, a + b
+
+    return b
+
+n = int(input("Enter the number of stairs: "))
+print("Number of ways:", climbStairs(n))
